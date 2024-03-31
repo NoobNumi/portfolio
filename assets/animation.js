d8b2fc96537bf22f96fcc14cd0f6e1e63e541b59
NoobNumi <numinum1128@gmail.com>
2024-03-31 13:42:22 +0800

    Add scroll position storage and restoration functions

--- assets/animation.js
+++ assets/animation.js
@@ -20,10 +20,42 @@ function updateHeaderAnimation() {
   } else {
     header.classList.remove('slide-up');
   }
-
-
 }
 
 window.addEventListener('resize', updateHeaderAnimation);
 updateHeaderAnimation();
 
+// Store current scroll position before closing offcanvas
+let lastScrollPosition = 0;
+
+function storeScrollPosition() {
+  lastScrollPosition = window.scrollY || window.pageYOffset;
+}
+
+function restoreScrollPosition() {
+  window.scrollTo(0, lastScrollPosition);
+}
+
+// Custom function to open offcanvas
+function openOffcanvas() {
+  const offcanvas = document.getElementById('offcanvasExample');
+  const offcanvasBackdrop = document.querySelector('.offcanvas-backdrop');
+
+  if (offcanvas && offcanvasBackdrop) {
+    offcanvas.classList.add('show');
+    offcanvasBackdrop.classList.add('show');
+    storeScrollPosition();
+  }
+}
+
+// Custom function to close offcanvas
+function closeOffcanvas() {
+  const offcanvas = document.getElementById('offcanvasExample');
+  const offcanvasBackdrop = document.querySelector('.offcanvas-backdrop');
+
+  if (offcanvas && offcanvasBackdrop) {
+    offcanvas.classList.remove('show');
+    offcanvasBackdrop.classList.remove('show');
+    restoreScrollPosition();
+  }
+}
